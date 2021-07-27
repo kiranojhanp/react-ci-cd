eec3fe2baddec09fcd90b7f0e81d537fefe7d5b0
Restyled.io <commits@restyled.io>
2021-07-27 03:18:58 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -11,12 +11,16 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-  className = "App-link"
-  href = "https://reactjs.org"
-  target = "_blank"
-  rel = "noopener noreferrer" > Deploy react using CI - CD</a>
-      </header><
-        /div>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Deploy react using CI - CD
+        </a>
+      </header>
+    </div>
   );
 }
 
