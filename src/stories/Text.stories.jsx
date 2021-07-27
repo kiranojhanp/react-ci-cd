5b7c07118c95a41d9d0380ffc6adf99182c95315
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2021-07-27 13:28:22 +0545

    Storybook added

---
+++ src/stories/Text.stories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Text } from "./Text";
+
+export default {
+	title: "Text Component",
+	component: Text,
+};
+
+const Template = (args) => <Text {...args} />;
+
+export const Red = Template.bind({});
+Red.args = {
+  color: 'red'
+}
+
+export const Blue = Template.bind({});
+Blue.args = {
+	color: "blue",
+};
